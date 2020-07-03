ae3213c33c4953504f636de64f4fe035d704b69a
K Dheeraj  Kumar <56029049+DheerajKumar97@users.noreply.github.com>
2020-07-03 15:02:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"engineerdheeraj97@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
